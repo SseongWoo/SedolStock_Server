224ceb7e989185910bad3c53306e27385db0005f
SseongWoo <csw10211@gmail.com>
2024-11-09 17:58:19 +0900

    1109_9

--- auto/env.js
+++ auto/env.js
@@ -1,5 +1,13 @@
 import dotenv from 'dotenv';
-dotenv.config({ path: '/home/ec2-user/sp/SP/.env' }); // .env 파일 로드
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// 현재 파일의 디렉터리 이름을 얻기
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// .env 파일 경로 설정
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 // 변수 선언
 const serviceAccountURL = process.env.FIREBASE_ADMIN_AUTO_SERVICE_ACCOUNT;
