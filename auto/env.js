658d0f04576f68278211068f18d2d0df7bb0d69c
SseongWoo <csw10211@gmail.com>
2024-11-09 17:49:10 +0900

    1109_7

--- auto/env.js
+++ auto/env.js
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
+console.log(serviceAccountURL, storageBucketURL, apiKey, channelIdList);
 
 export const serviceAccountURL = process.env.FIREBASE_ADMIN_AUTO_SERVICE_ACCOUNT;
 export const storageBucketURL = process.env.FIREBASE_ADMIN_STORAGE_BUCKET;
 export const apiKey = process.env.YOUTUBE_API_KEY;
 export const channelIdList = process.env.CHANNEL_ID_LIST.split(',');
 
-console.log(serviceAccountURL, storageBucketURL, apiKey, channelIdList);
