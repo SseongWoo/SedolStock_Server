d2f8e0f96583aa5c50f63f8c5231c45c701cdffc
SseongWoo <csw10211@gmail.com>
2024-11-09 17:51:27 +0900

    1109_8

--- auto/env.js
+++ auto/env.js
@@ -1,9 +1,15 @@
 import dotenv from 'dotenv';
-dotenv.config({ path: '../.env' });
+dotenv.config({ path: '../.env' }); // .env 파일 로드
+
+// 변수 선언
+const serviceAccountURL = process.env.FIREBASE_ADMIN_AUTO_SERVICE_ACCOUNT;
+const storageBucketURL = process.env.FIREBASE_ADMIN_STORAGE_BUCKET;
+const apiKey = process.env.YOUTUBE_API_KEY;
+const channelIdList = process.env.CHANNEL_ID_LIST ? process.env.CHANNEL_ID_LIST.split(',') : [];
+
+// 환경 변수 값 디버깅
 console.log(serviceAccountURL, storageBucketURL, apiKey, channelIdList);
 
-export const serviceAccountURL = process.env.FIREBASE_ADMIN_AUTO_SERVICE_ACCOUNT;
-export const storageBucketURL = process.env.FIREBASE_ADMIN_STORAGE_BUCKET;
-export const apiKey = process.env.YOUTUBE_API_KEY;
-export const channelIdList = process.env.CHANNEL_ID_LIST.split(',');
+// 변수 export
+export { serviceAccountURL, storageBucketURL, apiKey, channelIdList };
 
