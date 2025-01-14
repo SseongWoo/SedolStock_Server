d8756876c2f71239efcee28526d144a81abb5563
SseongWoo <csw10211@gmail.com>
2025-01-14 17:46:33 +0900

    ranking Update

--- utils/date.js
+++ utils/date.js
@@ -12,11 +12,12 @@ export function getTime() {
 export function getTime2() {
     const now = new Date();
     const year = now.getFullYear();
-    const month = now.getMonth() + 1;
-    const day = now.getDate();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // 두 자리로 패딩
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
